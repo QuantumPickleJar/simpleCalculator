721320c0dea1d4d1cd7bdf7b066f3a89ba4c87b0
vmorrilll <vmorrilll@gmail.com>
2018-09-19 18:56:57 -0500

    replaced declarations with TryParse() statements

--- simpleCalculator/simpleCalculator/MainPage.xaml.cs
+++ simpleCalculator/simpleCalculator/MainPage.xaml.cs
@@ -21,23 +21,46 @@ namespace simpleCalculator
             String strDay = dayEntry.Text;
 
             int iMonth, iDay, iYear;
-            iMonth = int.Parse(strMonth);
-            iDay = int.Parse(strDay);
-            iYear = int.Parse(strYear);
 
-            DateTime birthDate = new DateTime(iYear, iMonth, iDay);
+            if (int.TryParse(strMonth, out iMonth))
+            {
+                if (int.TryParse(strYear, out iYear))
+                {
+                    if (int.TryParse(strDay, out iDay))
+                    {
+                        DateTime birthDate = new DateTime(iYear, iMonth, iDay);
+                        TimeSpan age = DateTime.Now - birthDate;
+                        int numDays = age.Days;
+                        int totYears = numDays / 365;
+
+                        ageLabel.Text = numDays.ToString();
+                    }
+                    else
+                    {
+                        ageLabel.Text = "Please enter valid day (1-31)";
+                    }
+                }
+                else
+                {
+                    ageLabel.Text = "Please enter valid year"
+                    //let user know 
+                }
+            }
+
+            //iMonth = int.Parse(strMonth);
+            //iDay = int.Parse(strDay);
+            //iYear = int.Parse(strYear);
+
+            //DateTime birthDate = new DateTime(iYear, iMonth, iDay);
             //System.DateTime birthDate = new System.DateTime(iYear, iMonth, iYear);
             //what we would use if we didn't have "using System;"
 
 
-            var age = DateTime.Now - birthDate; //in this instance, 'age' will turn into variable type TimeSpan 
+            //var age = DateTime.Now - birthDate; //in this instance, 'age' will turn into variable type TimeSpan 
                                                 //var determines what datatype is most appropariate
                                                 //DateTime.Today;
 
-            int numDays = age.Days;
-            int totYears = numDays / 365;
-
-            ageLabel.Text = numDays.ToString();
+            
         }
     }
 }
